0fa01a53bcdb3aa162387ddaec07751e635d7b14
AntonGames <anton.zagzin@gmail.com>
2023-12-28 19:07:07 +0200

    Date error fixed

--- src/main/java/com/vu/localhost/poss/RFC3339DateFormat.java
+++ src/main/java/com/vu/localhost/poss/RFC3339DateFormat.java
@@ -1,19 +1,19 @@
 package com.vu.localhost.poss;
 
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.databind.util.ISO8601Utils;
 import java.text.FieldPosition;
 import java.util.Date;
-import java.text.SimpleDateFormat;
 
-public class RFC3339DateFormat extends SimpleDateFormat {
+public class RFC3339DateFormat extends ISO8601DateFormat {
 
   private static final long serialVersionUID = 1L;
 
-  // Same as SimpleDateFormat but serializing milliseconds.
+  // Same as ISO8601DateFormat but serializing milliseconds.
   @Override
   public StringBuffer format(Date date, StringBuffer toAppendTo, FieldPosition fieldPosition) {
-    String value = super.format(date);
+    String value = ISO8601Utils.format(date, true);
     toAppendTo.append(value);
     return toAppendTo;
   }
-
 }
